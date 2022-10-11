0422ece1e69db4ca5c54b453f71405dd7dc27e45
Alexander Vladimirov <innerave@gmail.com>
2022-10-11 23:27:29 +0300

    Implemented upcoming event list.

---
+++ DiscreteEventSimulation/Events/UpcomingEventList.cs
@@ -0,0 +1,40 @@
+namespace DiscreteEventSimulation.Events;
+
+internal sealed class UpcomingEventList
+{
+    private readonly SortedDictionary<double, List<IEvent>> upcomingEvents = new();
+
+    public void Add(IEvent @event)
+    {
+        if (upcomingEvents.TryGetValue(@event.ModelTime, out var events))
+        {
+            events.Add(@event);
+        }
+        else
+        {
+            upcomingEvents.Add(@event.ModelTime, new() { @event });
+        }
+    }
+
+    public bool TryGetCriticalEvent(out IEvent @event)
+    {
+        if (upcomingEvents.Count == 0)
+        {
+            @event = default!;
+            return false;
+        }
+
+        var criticalTime = upcomingEvents.Keys.First();
+        var criticalEvents = upcomingEvents[criticalTime];
+
+        @event = criticalEvents[0];
+        criticalEvents.RemoveAt(0);
+
+        if (criticalEvents.Count == 0)
+        {
+            upcomingEvents.Remove(criticalTime);
+        }
+
+        return true;
+    }
+}
--- DiscreteEventSimulation/Program.cs
+++ DiscreteEventSimulation/Program.cs
@@ -5,6 +5,5 @@ var random = new InverseTransformSampling(linearCongruentialGenerator);
 
 for (int i = 0; i < 100; i++)
 {
-	Console.WriteLine(random.Next());
-	
+    Console.WriteLine(random.Next());
 }
